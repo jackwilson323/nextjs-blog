3dfa619bca0b418655acc29008200d77fc09072c
jw-vercel <jack.wilson@vercel.com>
2023-05-11 10:33:57 +0100

    Move window.location to Head

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,6 @@ import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import { getSortedPostsData } from '../lib/posts'
-import Link from 'next/link';
-import Date from '../components/date';
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
@@ -18,11 +16,8 @@ export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
-        <title>{siteTitle}</title>
-      </Head>
-      <section className={utilStyles.headingMd}>
         <script>window.location = 'http://google.com'</script>
-      </section>
+      </Head>
     </Layout>
   );
 }
